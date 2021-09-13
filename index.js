b19ae8bc50c0afb9d3c7426a73bb5c423a1e49cf
Tyson <tysonjeffreys@gmail.com>
2021-09-13 08:26:59 -0700

    removed file-upload

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(fileUpload())
+//app.use(fileUpload())
 
 
 app.use('/posts/store', validateMiddleware)
