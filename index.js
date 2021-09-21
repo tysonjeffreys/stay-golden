7c7a41ece36328bcf97a79d3f937fb3225898063
Tyson <tysonjeffreys@gmail.com>
2021-09-21 06:47:46 -0700

    update

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(fileUpload())
+//app.use(fileUpload())
 
 
 
