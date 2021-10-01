957c99c087fce37c3c0cb02347b57e9093c30547
Tyson <tysonjeffreys@gmail.com>
2021-10-01 06:43:44 -0700

    continue adding pagination

--- controllers/homeData.js
+++ controllers/homeData.js
@@ -3,10 +3,36 @@ const BlogPost = require('../models/BlogPost.js')
 module.exports = async (req, res) => {
     let perPage = 5
     let page = 1
-    
+    /*
     const blogposts = await BlogPost.find({})
     .skip((perPage * page) - perPage)
         .limit(perPage)
+        .sort({$natural:1})
+*/
+
+    const imageposts = await BlogPost.aggregate([
+        { $facet: 
+            {
+            "totalData": [
+                { $match : {} }
+            ],
+            
+            "totalCount": [
+                { $count : 'count' }
+            ]
+            }
+        }
+    ]) 
+    console.log(imageposts[0])
+    
+        
+        /*
+        .exec((err, posts) => {
+            posts.reverse();
+            res.send(posts)
+                    
+            //callback(err, result)
+        })*/
         //.exec(function(err, products) {
         //    Product.count().exec(function(err, count) {
         //        if (err) return next(err)//.populate('userid')
@@ -14,7 +40,7 @@ module.exports = async (req, res) => {
         //console.log(blogposts)
         //res.render('index', {
         //    blogposts
-                res.send(blogposts)    
+                res.send(imageposts)    
     //});
        //     })
         //})
